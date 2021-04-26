e66a4e8760b41ae7bbbb99abb851cbb7f6885055
Nick Nack Attack <nickfarni001@gmail.com>
2021-04-26 11:42:24 -0600

    add NoAudioMessage

---
+++ src/components/NoAudioMessage/NoAudioMessage.tsx
@@ -0,0 +1,8 @@
+export const NoAudioMessage = () => {
+    return (
+        <div>
+            <p>Sorry, this isn't going to work out</p>
+            <p>Seems like your browser doesn't support <code>Audio API</code>.</p>
+        </div>
+    )
+}
