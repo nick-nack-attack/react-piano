2e183b9226a2d9e272032b1932010b0e2aa59d2a
Nick Nack Attack <nickfarni001@gmail.com>
2021-04-26 11:51:56 -0600

    add keyboard

---
+++ src/domain/keyboard.ts
@@ -0,0 +1,20 @@
+import { OctaveIndex, PitchIndex } from "./note";
+
+export type Key = string;
+export type Keys = Key[];
+
+export const TOP_ROW: Keys = Array.from("q2w3er5t6y7u");
+export const BOTTOM_ROW: Keys = Array.from("zsxdcvgbhnjm");
+
+/**
+ * Key: a type-alias for representing letter key labels
+ *
+ * Keys: an array of those labels
+ * */
+export function selectKey(
+    octave: OctaveIndex,
+    index: PitchIndex
+): Key {
+    const keysRow = octave < 5 ? TOP_ROW : BOTTOM_ROW
+    return keysRow[index]
+}
